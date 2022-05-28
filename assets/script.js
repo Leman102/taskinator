8edfef39e16ffa3d74b5becfc9fec30700cb5cb7
Leoman <leo.pinzon102@gmail.com>
2022-05-28 18:21:03 -0400

    add list items and new div in js

--- assets/script.js
+++ assets/script.js
@@ -16,10 +16,25 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event){
     //prevent to refresh the webpage in the browser as sson as we run this code
     event.preventDefault();
+    /*When we use square brackets [ ] in a selector, we're trying to select an HTML element by one of its attributes. 
+    In this case, we're selecting the <input> element on the page that has a name attribute set to a value of "task-name".*/
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // console.dir(taskTypeInput);
     var listItemEL = document.createElement("li");
     listItemEL.className = "task-item";
-    listItemEL.textContent = "This is a new task";
+    //add the info from the form 
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEL.appendChild(taskInfoEl);
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEL);
+    console.dir(listItemEL);
 }
 //add event listener to add a new item
 
